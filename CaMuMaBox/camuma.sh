376a93a2181fb557e7c9cb8b9cb0787b968bbe0d
Calexo <calexo@calexo.com>
2013-03-15 14:59:05 +0100

    Enhance fade in/out

--- CaMuMaBox/camuma.sh
+++ CaMuMaBox/camuma.sh
@@ -6,6 +6,7 @@ BC="`which bc` -l"
 NORMAL_VOLUME=80
 FADED_VOLUME=30
 FADE_TIME=2
+SLEEP=0.1
 
 fadeout() {
 	VOLUME=$(($NORMAL_VOLUME - 5))
@@ -13,7 +14,7 @@ fadeout() {
 	do
 	    $MPC volume $VOLUME > /dev/null
 	    VOLUME=$(($VOLUME - 5))
-	    sleep `echo "$FADE_TIME/(5*($NORMAL_VOLUME - $FADED_VOLUME))" | $BC`
+	    sleep $SLEEP
 	    mpc volume
 	done
 }
@@ -24,7 +25,8 @@ fadein() {
 	do
 	    $MPC volume $VOLUME > /dev/null
 	    VOLUME=$(($VOLUME + 5))
-	    sleep `echo "$FADE_TIME/(5*($NORMAL_VOLUME - $FADED_VOLUME))" | $BC`
+	    #sleep `echo "$FADE_TIME/(5*($NORMAL_VOLUME - $FADED_VOLUME))" | $BC`
+	    sleep $SLEEP
 	    mpc volume
 	done
 }
