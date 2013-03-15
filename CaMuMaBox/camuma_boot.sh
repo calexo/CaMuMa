556e26a624166e5e7a34fab8af4fd841ac4e234a
Calexo <calexo@calexo.com>
2013-03-15 14:58:31 +0100

    Boot repair

--- CaMuMaBox/camuma_boot.sh
+++ CaMuMaBox/camuma_boot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-sudo service mpd stop
+#sudo service mpd stop
 #while [ ping 192.168.1.112 -c 2 2>/dev/null >/dev/null ]
 #do
 #        echo "CaMuMa - Waiting network..."
@@ -9,11 +9,16 @@ sudo service mpd stop
 #sleep 3
 #sudo mount /mnt/music
 echo "CaMuMa - Reloading server..."
-sudo service mpd start
+#sudo service mpd start
 mpc pause
+mpc volume 80
 echo "CaMuMa - Updating database..."
-#sudo dos2unix -n /mnt/music/camuma.lst /mnt/music/camuma.lst.unix &
+sudo dos2unix -n /media/USB/camuma.lst /home/pi/camuma.lst.unix &
 mpc update
 ~/camuma_up.sh &
 python ~/daemon.py &
-~/qrdecode.sh &
+#~/qrdecode.sh &
+
+aplay ~/Lightning.wav
+
+~/camumad.sh
